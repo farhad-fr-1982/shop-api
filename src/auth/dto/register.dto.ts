2e83a6b4c984368fb15fa232645e01d62931d814
Farhad <farhad.fr@outlook.com>
2026-04-24 10:41:15 +0330

    auth-dto

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,8 +1,7 @@
-import { IsEnum, IsNotEmpty, IsOptional, IsString, Length, Matches, MaxLength, MinLength } from "class-validator";
-import UserRoleEnum from "../enums/userRoleEnum";
 import { Transform } from "class-transformer";
+import { IsEnum, IsNotEmpty, IsOptional, IsString, Length, Matches, MaxLength, MinLength } from "class-validator";
 
-export class CreateUserDto {
+export class RegisterDto {
     @IsString({ message: 'شماره همراه باید یک رشته باشد' })
     @Length(11, 11, { message: 'شماره همراه باید 11 کاراکتر باشد' })
     @Matches(/^09[0-9]{9}$/, { message: 'شماره همراه معتبر نیست' })
@@ -10,17 +9,13 @@ export class CreateUserDto {
     @Transform(({ value }) => value.trim())
     mobile: string;
 
-    @IsString({ message: 'نام باید یک رشته باشد' })
-    @IsNotEmpty({ message: 'نام نمی‌تواند خالی باشد' })
-    display_name: string;
-
     @IsString({ message: 'کلمه عبور باید یک رشته باشد' })
     @MinLength(6, { message: 'کلمه عبور باید حداقل 6 کاراکتر باشد' })
     @MaxLength(15, { message: 'کلمه عبور باید حداکثر 15 کاراکتر باشد' })
     @IsNotEmpty({ message: 'کلمه عبور نمی تواند خالی باشد' })
     password: string;
 
-    @IsOptional()
-    @IsEnum(UserRoleEnum, { message: 'نقش کاربر باید user یا admin باشد' })
-    role?: UserRoleEnum;
+    @IsString({ message: 'نام باید یک رشته باشد' })
+    @IsNotEmpty({ message: 'نام نمی‌تواند خالی باشد' })
+    display_name: string;
 }
